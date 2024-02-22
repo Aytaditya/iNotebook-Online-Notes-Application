90aae0bd2145a8c2fbf693b92c004c8d6a7bc9c4
Aytaditya <adityaaryan531@gmail.com>
2024-02-23 02:09:49 +0530

    first commit

---
+++ backend/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const mongoUri = "mongodb://0.0.0.0:27017";
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to my MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
+};
+
+module.exports = connectToMongo;
---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const connectToMongo=require('./db');
+connectToMongo();
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello aditya!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
+
+
