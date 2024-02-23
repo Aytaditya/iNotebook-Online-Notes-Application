963cb6c382355a979f251d70e91137d2d6e0e409
Aytaditya <adityaaryan531@gmail.com>
2024-02-24 00:38:03 +0530

    third commit

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,12 @@ const port = 3000
 //   res.send('Hello aditya aryan!')
 // })
 
+
+//you cant directly use req.body you need a middle way to use it 
+app.use(express.json())
+
+
+
 //putting all routes like inotes/auth and inotes/login or inotes/about in routes folder 
 app.use('/api/auth',require('./routes/auth.js'))
 app.use('/api/notes',require('./routes/notes.js'))
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,32 @@
 const express=require('express')
 const router=express.Router()
+const User=require('../models/User')
+const {body,validationResult}=require('express-validator')
 
-router.get('/',(req,res)=>{
-    obj={
-        a:"the",
-    number:134
-    }
-    res.json(obj)
+//you cant directly use req.body you need a middle way to use it 
+
+
+//create a user using POST "/api/auth"
+
+//use router.post not royer.get so that the password is saved 
+router.post('/',[
+    //for correct entry checking
+    body('email').isEmail(),
+    body('name').isLength({min:8}),
+    body('password').isLength({min:8})
+],(req,res)=>{
+//     console.log(req.body)
+//   const user=User(req.body);
+
+//   //for saving data in mongoDb
+//   user.save()
+//       or 
+  const errors=validationResult(req)
+  if(!errors.isEmpty()){
+    return res.status(400).json({errors:errors.array()});
+  }
+  //simply if errors is not empty return error else save data 
+  res.send(req.body)
+  
 })
 module.exports=router
