7b1374566a34c1b1f3650ae6f557b5ece6260272
Aytaditya <adityaaryan531@gmail.com>
2024-02-24 21:41:02 +0530

    sixth commit

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,14 +2,21 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator')
+//hashing password using bcrypt
+var bcrypt = require('bcryptjs');
+var jwt=require('jsonwebtoken');
+
+//ideally we have to hid this string in our environment variable
+const JWT_SECRET='adityaisgood';
+
 
 //you cant directly use req.body you need a middle way to use it 
 //middleman created in index.js file
 
 
-//create a user using POST "/api/auth/createuser"
+//creating a user using POST "/api/auth/createuser"
 //use router.post not roter.get so that the password is safe
-//royer.post gives safty
+//router.post gives safty
 router.post('/createuser', [
     //for correct entry checking though express validator 
     body('email', 'Enter a valid Email').isEmail(),
@@ -20,7 +27,7 @@ router.post('/createuser', [
     //     console.log(req.body)
     //   const user=User(req.body);
 
-    //   //for saving data in mongoDb
+    //   for saving data in mongoDb
     //   user.save()
     //       or 
     const errors = validationResult(req)
@@ -35,10 +42,18 @@ router.post('/createuser', [
             return res.status(400).json({ error: "Account already exist" })
         }
 
+
+        //insted of using password:req.body.password we will use bcryptjs
+        //generating salt and adding to password for extra safety of user
+        // we use await because it returns promise 
+        const salt= await bcrypt.genSalt(10);
+        secPass= await bcrypt.hash(req.body.password,salt);
+
+        //creating a new user
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: secPass
 
         })
 
@@ -47,13 +62,74 @@ router.post('/createuser', [
         //   .then(res.send(req.body))
         //   .catch(err=> console.log(err),
         //   res.json({error:"please enter email correctly"}))
-        res.json(user)
+
+        // we will not return user details to user we will return a token which will be used in signining 
+        // we will use jsonwebtoken
+        // -----> res.json(user)
+        const data={
+            user:{
+                id:user.id
+            }
+        }
+        //jwt.sign is a sunc method hence dont need to add await
+         const authToken= jwt.sign(data,JWT_SECRET)
+         // if we add {} we will be able to send response as authToken:
+         res.json({authToken})
+
+
     }
     catch (error) {
         console.log(error)
         res.status(500).send("Error Occured")
+    }
+})
 
+// authenticate a user 
+router.post('/login', [
+    //for correct entry checking though express validator 
+    body('email', 'Enter a valid Email').isEmail(),
+    body('password','Password cannot be blank').exists()
+], async (req, res) => {
+    
+    //sending back error if not true
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
 
+    // here talking about password, email entered by user 
+    const {email,password}=req.body
+    try {
+        //seraching in data base
+        let user= await User.findOne({email})
+        // returning bad response if user does not exist in database
+        if(!user){
+            return res.status(400).json({error:"Wrong Credentials Try Again"})
+        }
+        //comparing entered password to user password 
+        //passwordCompare will return true false 
+        //bcrypt is defined by me
+        const passwordCompare= await bcrypt.compare(password,user.password)
+
+        if(!passwordCompare){
+            return res.status(400).json({error:"Wrong Credentials Try Again"})
+        }
+        const data={
+            user:{
+                id:user.id
+            }
+        }
+        const authToken=jwt.sign(data,JWT_SECRET)
+        res.json({authToken})
+
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Internal Server Error")
+        
+    }
+
+
+
 })
+
 module.exports = router
